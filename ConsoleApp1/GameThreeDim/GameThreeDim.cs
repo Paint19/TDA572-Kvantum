e99617586c9296798574b4a38d45b76c9e9963d2
Paint19 <hanna@schaff.se>
2024-02-13 14:06:34 +0100

    Starting point for 3d implementation

---
+++ ConsoleApp1/GameThreeDim/GameThreeDim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    internal class GameThreeDim : Game, InputListener
+    {
+        public void handleInput(InputEvent inp, string eventType)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public override void initialize()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public override void update()
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp1/Shard/WindowOpenTK.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Shard
+{
+    internal class WindowOpenTK : GameWindow
+    {
+        public WindowOpenTK(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() 
+            { Size = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
