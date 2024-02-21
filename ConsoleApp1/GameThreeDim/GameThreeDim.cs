ed2b604d7d8b13192a6feaf1310a01615750e74c
GitForGood <oliversofta@gmail.com>
2024-02-21 15:29:16 +0100

    removed deprecated rotation math and added camera
    camera is non functional but has the necessary math to theoretically function

--- ConsoleApp1/GameThreeDim/GameThreeDim.cs
+++ ConsoleApp1/GameThreeDim/GameThreeDim.cs
@@ -1,8 +1,10 @@
-﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using Shard.Shard;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +14,7 @@ namespace Shard
     {
         float[] vertices;
         uint[] indices;
-
+        Matrix3 persistentRotationMatrix3;
         public void handleInput(InputEvent inp, string eventType)
         {
             // throw new NotImplementedException();
@@ -23,10 +25,19 @@ namespace Shard
             Tuple<float[], uint[]> parsedFile = ObjectFileParser.parseFile("rat.obj");
             vertices = parsedFile.Item1;
             indices = parsedFile.Item2;
-            rotateVertices(0.0f, 0.0f, 3.0f / (4.0f * (float)Math.PI));
+            Matrix3 rotMatrix = Matrices.getInstance().getRotationMatrix3(0.0f, 0.0f, 0.250f);
+            persistentRotationMatrix3 = Matrices.getInstance().getRotationMatrix3(0.0f, 0.01f, 0.0f);
+            rotateVertices(rotMatrix);
             // throw new NotImplementedException();
         }
-
+ 
+        void rotateVertices(Matrix3 rotMatrix)
+        {
+            this.vertices = vertices
+                .Chunk(3)
+                .Select(vec=> rotMatrix * new Vector3(vec[0], vec[1], vec[2]))
+                .SelectMany(nVec => new float[] { nVec[0], nVec[1], nVec[2] }).ToArray();
+        }
         public void rotateVertices(float a, float b, float c)
         {
             List<float[]> chunks = vertices.Chunk(3).ToList();
@@ -39,7 +50,7 @@ namespace Shard
 
         public override void update()
         {
-            rotateVertices(0.0f, 0.01f, 0.0f);
+            rotateVertices(persistentRotationMatrix3);
             Bootstrap.getDisplay().drawShape(vertices, indices);
         }
     }
