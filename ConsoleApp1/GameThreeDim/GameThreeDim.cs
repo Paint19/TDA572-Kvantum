8172060dc04f8fc16ee730e478ac9e4394d742e9
Paint19 <hanna@schaff.se>
2024-02-26 15:35:51 +0100

    Extended file parser to parse any obj file

--- ConsoleApp1/GameThreeDim/GameThreeDim.cs
+++ ConsoleApp1/GameThreeDim/GameThreeDim.cs
@@ -12,6 +12,7 @@ namespace Shard
 {
     class GameThreeDim : Game, InputListener
     {
+        Vector3[] verticesUnprocessed;
         float[] vertices;
         uint[] indices;
         Matrix3 persistentRotationMatrix3;
@@ -22,21 +23,32 @@ namespace Shard
 
         public override void initialize()
         {
-            Tuple<float[], uint[]> parsedFile = ObjectFileParser.parseFile("teapot.obj");
-            vertices = parsedFile.Item1;
-            indices = parsedFile.Item2;
+            ObjectFileParser parser = new ObjectFileParser("rat.obj");
+            verticesUnprocessed = parser.getVertices();
+            indices = parser.getIndices();
             Matrix3 rotMatrix = Matrices.getInstance().getRotationMatrix3(0.0f, 0.0f, 0.250f);
             persistentRotationMatrix3 = Matrices.getInstance().getRotationMatrix3(0.0f, 0.01f, 0.0f);
             rotateVertices(rotMatrix);
             // throw new NotImplementedException();
         }
- 
+
         void rotateVertices(Matrix3 rotMatrix)
         {
-            this.vertices = vertices
+            if (verticesUnprocessed != null)
+            {
+                this.vertices = verticesUnprocessed
+                    .Select(vec => rotMatrix * vec)
+                    .SelectMany(nVec => new float[] { nVec[0], nVec[1], nVec[2] }).ToArray();
+
+                verticesUnprocessed = null;
+            }
+            else
+            {
+                this.vertices = vertices
                 .Chunk(3)
                 .Select(vec=> rotMatrix * new Vector3(vec[0], vec[1], vec[2]))
                 .SelectMany(nVec => new float[] { nVec[0], nVec[1], nVec[2] }).ToArray();
+            }
         }
 
         public override void update()
--- ConsoleApp1/Shard/ObjectFileParser.cs
+++ ConsoleApp1/Shard/ObjectFileParser.cs
@@ -16,11 +16,31 @@ namespace Shard.Shard
          *  parseFile takes a file name (file must be placed in Asset-folder) and 
          *  parses it as an object file.
          */
-        static public Tuple<float[], uint[]> parseFile(string fileName)
+
+        private Vector3[] vertices;
+        private uint[] indices;
+        private uint[] textureIndices;
+        private uint[] normalIndices;
+        private float[] verticeColor;
+        private Vector3[] textureCoordinates;
+        private Vector3[] vertexNormals;
+        private Vector3[] parameterSpaceCoordinates;
+        private uint[] lineElements;
+
+
+        public ObjectFileParser(string fileName)
         {
             string filePath = Bootstrap.getAssetManager().getAssetPath(fileName);
-            List<float> vertices = new List<float>();
-            List<uint> indices = new List<uint>();
+
+            List<Vector3> verts = new List<Vector3>();
+            List<uint> vertInds = new List<uint>();
+            List<uint> texInds = new List<uint>();
+            List<uint> normInds = new List<uint>();
+            List<float> vertCol = new List<float>();
+            List<Vector3> texCoords = new List<Vector3>();
+            List<Vector3> vertNorms = new List<Vector3>();
+            List<Vector3> paramSpaceCoords = new List<Vector3>();
+            List<uint> lineElems = new List<uint>();
 
             IEnumerable<string> allLines;
 
@@ -31,30 +51,93 @@ namespace Shard.Shard
 
                 foreach (string line in allLines)
                 {
-                    string[] vals = line.Split(' ');
+                    string[] words = line.Split(' ');
 
-                    if (vals[0].Equals("v")) {
-                        for(int i = 1; i < vals.Length; i++)
-                        {
-                            vertices.Add(float.Parse(vals[i], CultureInfo.InvariantCulture.NumberFormat)  * 0.1f);  //TODO: Remove *0.1f // Must be in "0.00..." format);
-                        }
-                    }
-                    else if (vals[0].Equals("f"))
+                    switch (words[0])
                     {
-                        for (int i = 1; i < vals.Length; i++)
-                        {
-                            uint indOneIndexed = uint.Parse(vals[i], CultureInfo.InvariantCulture.NumberFormat); // .obj file indices are one indexed, while our indices should be zero indexed
-                            uint indZeroIndexed = indOneIndexed - 1;
-                            indices.Add(indZeroIndexed);
-                        }
+                        case "v": // Geometric vertices. x, y, z, w. w defaults to 1 and defines a color value ranging 0 to 1.
+                            verts.Add(
+                            new Vector3(
+                                float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[2], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat)) * 0.001f);  //TODO: Remove *0.1f
+
+                            if (words.Length == 5) // w
+                                vertCol.Add(float.Parse(words[4], CultureInfo.InvariantCulture.NumberFormat));
+                            break;
+
+                        case "vt": // Texture coordinates. u, v, w coordinates, these will vary between 0 and 1. v, w are optional and default to 0.
+                            float first = float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat);
+                            float second = 0;
+                            float third = 0;
+                            if (words.Length > 2)
+                                second = float.Parse(words[2], CultureInfo.InvariantCulture.NumberFormat);
+                            if (words.Length > 3)
+                                third = float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat);
+                            texCoords.Add(new Vector3(first, second, third));
+                            break;
+
+                        case "vn": // Vertex normals in (x,y,z) form. Normals might not be unit vectors.
+                            vertNorms.Add(
+                            new Vector3(
+                                float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[2], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat)));
+                            break;
+
+                        case "vp": // Parameter space vertice. In u, v, w form; free form geometry statement 
+                            paramSpaceCoords.Add(
+                            new Vector3(
+                                float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[2], CultureInfo.InvariantCulture.NumberFormat),
+                                float.Parse(words[3], CultureInfo.InvariantCulture.NumberFormat)));
+                            break;
+
+                        case "f": // Face element. In vertex_index/texture_index/normal_index form. Texture and normal indices are optional
+                            for (int i = 1; i < words.Length; i++)
+                            {
+                                string[] inds = words[i].Split("//", '/');
+
+                                vertInds.Add(uint.Parse(inds[0], CultureInfo.InvariantCulture.NumberFormat) - 1);
+
+                                if (words[i].Contains("//"))
+                                    normInds.Add(uint.Parse(inds[1], CultureInfo.InvariantCulture.NumberFormat) - 1);
+                                else if (inds.Length > 1)
+                                    texInds.Add(uint.Parse(inds[1], CultureInfo.InvariantCulture.NumberFormat) - 1);
+                                if (inds.Length > 2)
+                                    normInds.Add(uint.Parse(inds[2], CultureInfo.InvariantCulture.NumberFormat) - 1);
+                            }
+                            break;
+
+                        case "l": // Line element, these specify the order of the vertices which build a polyline. 
+                            for (int i = 1; i < words.Length; i++)
+                                lineElems.Add(uint.Parse(words[i], CultureInfo.InvariantCulture.NumberFormat));
+                            break;
                     }
+
+                    // Set all values here
+                    vertices = verts.ToArray();
+                    indices = vertInds.ToArray();
+                    textureIndices = texInds.ToArray();
+                    normalIndices = normInds.ToArray();
+                    verticeColor = vertCol.ToArray();
+                    textureCoordinates = texCoords.ToArray();
+                    vertexNormals = vertNorms.ToArray();
+                    parameterSpaceCoordinates = paramSpaceCoords.ToArray();
+                    lineElements = lineElems.ToArray();
                 }
             }
-
-            float[] vs = vertices.ToArray();
-            uint[] inds = indices.ToArray();
-
-            return new Tuple<float[], uint[]>(vs, inds);
         }
+        
+        public Vector3[] getVertices() { return vertices; }
+        public uint[] getIndices() { return indices; }
+        public uint[] getTextureIndices() { return textureIndices; }
+        public uint[] getNormalIndices() {  return normalIndices; }
+        public float[] getVerticeColor() { return verticeColor; }
+        public Vector3[] getTextureCoordinates() { return textureCoordinates; }
+        public Vector3[] getVertexNormals() { return vertexNormals; }
+        public Vector3[] getParameterSpaceCoordinates() { return parameterSpaceCoordinates; }   
+        public uint[] getLineElements() { return lineElements; }    
+
     }
 }
