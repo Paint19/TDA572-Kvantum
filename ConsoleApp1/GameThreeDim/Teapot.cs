2f16d21cf6dd92a663dbff3b3c381ab36bbd6ac0
Paint19 <hanna@schaff.se>
2024-03-04 16:33:47 +0100

    Added functionality to render multiple objects

---
+++ ConsoleApp1/GameThreeDim/Teapot.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    class Teapot : GameObject
+    {
+        Matrix3 persistentRotationMatrix3 = Matrices.getInstance().getRotationMatrix3(0.0f, 0.01f, 0.0f);
+        float moveDirection;
+
+        public Teapot(float dir)
+        {
+            this.Transform.initRenderer("teapot.obj");
+            Bootstrap.getDisplay().addToDraw(this);
+            this.Transform.tmpChangeSize(0.1f);
+            this.moveDirection = dir;
+        }
+        public override void initialize()
+        {
+            base.initialize();
+        }
+
+        public override void update()
+        {
+            base.update();
+            this.Transform.rotateVertices(persistentRotationMatrix3);
+            this.Transform.tmpMove(moveDirection);
+        }
+    }
+}
