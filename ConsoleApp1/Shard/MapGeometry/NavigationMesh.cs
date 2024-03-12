e271b9f14eebb8d3e4772c2e3f7027eab2aaa73a
GitForGood <oliversofta@gmail.com>
2024-03-12 20:21:04 +0100

    Starting to implement Nav mesh
    Should keep track of its occupants to allow for pathfinding

---
+++ ConsoleApp1/Shard/MapGeometry/NavigationMesh.cs
@@ -0,0 +1,14 @@
+﻿using Shard.pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.MapGeometry
+{
+    class NavigationMesh : Graph<InvisibleMat>
+    {
+
+    }
+}
